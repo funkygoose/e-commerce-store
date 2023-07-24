b855411403c48cc694d09db32df8f18bc7ee66bc
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-07-23 23:39:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 function App() {
@@ -5,6 +6,7 @@ function App() {
     <div className="App">
       <Nav/>
       <Landing />
+      <Highlights />
     </div>
   );
 }
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import Highlight from "./ui/Highlight";
 
 const Highlights = () => {
   return (
@@ -10,33 +11,22 @@ const Highlights = () => {
             why choose <span className="purple">Library</span>
           </h2>
           <div className="highlight__wrapper">
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="bolt" />
-              </div>
-              <h3 className="highlight__subtitle">Easy and Quick</h3>
-              <p className="highlight__para">
-                Get access to the book you purchased online instantly.
-              </p>
-            </div>
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="book-open" />
-              </div>
-              <h3 className="highlight__subtitle">10,000+ Books</h3>
-              <p className="highlight__para">
-                Library has books in all your favorite categories.
-              </p>
-            </div>
-            <div className="highlight">
-              <div className="highlight__img">
-                <FontAwesomeIcon icon="tags" />
-              </div>
-              <h3 className="highlight__subtitle">Afforable</h3>
-              <p className="highlight__para">
-                Get your hands on popular books for as little as $10.
-              </p>
-            </div>
+            <Highlight
+              icon={<FontAwesomeIcon icon="bolt" />}
+              title="Easy and Quick"
+              para="Get access to the book you purchased online instantly."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="tags" />}
+              title="Afforable"
+              para="Get your hands on popular books for as little as $10."
+            />
+            <Highlight
+              icon={<FontAwesomeIcon icon="book-open" />}
+              title="10,000+ Books"
+              para="Library has books in all your favorite categories."
+            />
+            
           </div>
         </div>
       </div>
