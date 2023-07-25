7f2dc64939b92c83ef0c2ed11f5a16a1797cbe08
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-07-25 10:13:32 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
+
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Route />
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" component={Books} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
         <div className="row row__column">
           <a href="/">
             <figure className="footer__logo">
-              <img src={Logo} class="footer__logo--img" alt="" />
+              <img src={Logo} className="footer__logo--img" alt="" />
             </figure>
           </a>
           <div className="footer__list">
             <a href="/" className="footer__link">Home</a>
             <span className="footer__link no-cursor">About</span>
-            <a href="/books" class="footer__link" > Cart</a>
+            <a href="/books" className="footer__link" > Cart</a>
           </div>
           <div className="footer__copyright">
             Copyright &copy; 2023 Library
