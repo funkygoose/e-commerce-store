22620f1acb9139dc6c846ed7fd11e80428c50e0b
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-07-25 00:14:36 -0700

    initial commit

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -11,7 +11,10 @@ const Discounted = () => {
             Discount <span className="purple">Books</span>
           </h2>
           <div className="books">
-            {books.map((book) => (
+            {books
+            .filter(book => book.salePrice > 0)
+            .slice(0, 8)
+            .map((book) => (
               <Book book={book} key={book.id} />
             ))}
           </div>
