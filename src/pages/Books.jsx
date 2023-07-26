cc99b10f53137f7108e706c062eab8a26233eab0
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-07-26 00:15:53 -0700

    initial commit

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,7 +1,32 @@
 import React, { useState } from "react";
 import Book from "../components/ui/Book";
 const Books = ({ books: initialBooks }) => {
-    const [books, setBooks] = useState(initialBooks);
+  const [books, setBooks] = useState(initialBooks);
+  function filterBooks(filter) {
+    console.log(filter);
+    if (filter === "LOW_TO_HIGH") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (a.salePrice || a.originalPrice) -
+              (b.salePrice || b.originalPrice)
+          )
+      );
+    }
+    if (filter === "HIGH_TO_LOW") {
+      setBooks(
+        books
+          .slice()
+          .sort(
+            (a, b) =>
+              (b.salePrice || b.originalPrice) -
+              (a.salePrice || a.originalPrice)
+          )
+      );
+    }
+  }
   return (
     <div id="books__main">
       <section>
@@ -9,7 +34,11 @@ const Books = ({ books: initialBooks }) => {
           <div className="row">
             <div className="books__header">
               <h2 className="section__title books__header--title">All Books</h2>
-              <select id="filter" defaultValue="DEFAULT">
+              <select
+                id="filter"
+                defaultValue="DEFAULT"
+                onChange={(event) => filterBooks(event.target.value)}
+              >
                 <option value="DEFAULT" selected disabled>
                   Sort
                 </option>
