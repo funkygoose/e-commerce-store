6c1c9a27ed90ab8694b71fe7c6f4557c4121ab91
Uriel Banuelos <40682078+funkygoose@users.noreply.github.com>
2023-07-28 14:44:05 -0700

    initial commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -31,8 +31,31 @@ const Cart = () => {
                       <button className="cart__book--remove">Remove</button>
                     </div>
                   </div>
+                  <div className="cart__quantity">
+                    <imput type="number" min={0} max={99} className="cart__input"></imput>
+                  </div>
+                  <div className="cart__total">
+                    $10.00
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="total">
+                <div className="total__item total__sub-total">
+                    <span>Subtotal</span>
+                    <span>$9.00</span>
+                </div>
+                <div className="total__item total__tax">
+                    <span>tax </span>
+                    <span>$1.00 </span>
+                </div>
+                <div className="total__item total__price">
+                    <span>Total </span>
+                    <span>$10.00 </span>
+                </div>
+                <button className="btn btn__checkout no-cursor" onClick={() => alert(`Haven't got around to doing this`)}>
+                    Proceed to checkout
+                </button>
             </div>
           </div>
         </div>
